5ed4f1dac5fdb24d3d4cba5f13fb93cff4a7321a
Yuri Chapyuk <yurachapyuk@gmail.com>
2024-02-23 13:01:20 +0200

    Finish learn course

--- src/components/EffectSection.jsx
+++ src/components/EffectSection.jsx
@@ -1,18 +1,29 @@
 import Button from "@/components/Button/Button.jsx"
 import Modal from "@/components/Modal/Modal"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 export default function EffectSection() {
 
   const [modal, setModal] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [users, setUsers] = useState([])
 
-  function openModal() {
-    setModal(true);
-  }
+  
+  useEffect(()=> {
+    async function fetchUsers() {
+      setLoading(true)
+      const response = await fetch('https://jsonplaceholder.typicode.com/users')
+      const users = await response.json()
+      setUsers(users)
+      setLoading(false)
+    }
+    fetchUsers()
+  }, [])
+
   return (
     <section>
       <h3>Effects</h3>
       <Button
-        onClick={openModal}
+        onClick={() => setModal(true)}
       >Open Modal</Button>
       <Modal open={modal}>
       <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center, '}}>
@@ -25,6 +36,10 @@ export default function EffectSection() {
         <hr />
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi deleniti quibusdam nemo quisquam eius, ad beatae, incidunt et deserunt ipsam ipsum tempore! Ipsam qui error eos debitis temporibus illum sint!</p>
       </Modal>
+      {loading && <p>Loading...</p>}
+      {!loading && <ul>
+        {users.map(user => <li className="card p-2 mb-2" key={user.id}>{user.name}</li>)}
+      </ul>}
     </section>
   )
 }
